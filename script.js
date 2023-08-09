63d7a5f9f0ba226f53299f097647fecbdeb2ac80
AntonioSTO <antoniostoliveira20@gmail.com>
2023-08-09 13:53:52 -0300

    new logic added

--- script.js
+++ script.js
@@ -1,76 +1,55 @@
-const outputElement = document.getElementById("output");
-const inputElement = document.getElementById("input");
-
-// Função para exibir mensagens no output
-function displayMessage(message) {
-  outputElement.innerHTML += `<p>${message}</p>`;
-}
-
-function what2do(bool){
-  displayMessage("O que você quer fazer agora? viajar, investigar ou conversar?");
-}
-
-var cidades = ["vitoria", "vila velha", "cariacica", "viana", "piuma"]
-  var viajar = 0;
-  var investigar = 0;
-  var conversar = 0;
-
-// Função principal do jogo
-function play() {
-  const command = inputElement.value.trim().toLowerCase();
-  
-  // Lógica do jogo aqui
-  switch (command) {
-    case "iniciar":
-      displayMessage("O jogo começou! Você é um detetive atrás de Carmen Sandiego.");
-      displayMessage("Para encontrá-la, você deve à procurar dentre os lugares do Espírito Santo, foi lá que Carmen foi vista da última vez!");
-      displayMessage("INVESTIGUE!!!")
-      what2do(true);
-      break;
-    
-    case "viajar":
-      if (viajar <= 5){
-        displayMessage("Na cidade de " + cidades[viajar] + " não há nada");
-        viajar += 1;
-      }
-      if (cidades[viajar] == "cariacica"){
-        displayMessage("Ela está aqui em cariacica!!!!");
-        viajar += 1;
-      }
-      if (viajar > 5){
-        displayMessage("Não adianta viajar mais");
-      }
-      break;
-
-    case "investigar":
-      if (investigar <= 4){
-        displayMessage("Procure mais um pouco, recruta");
-        investigar += 1;
-      }
-      else if (investigar == 5){
-        displayMessage("Você a encontrou, parabéns!!! Carmen estava em Jardim América")
-        displayMessage("Digite 'sair'")
-      }
-      break;
-
-    case "conversar":
-      displayMessage("Não adianta nada conversar com estranhos!!!");
-      break;
-
-    case "sair":
-      displayMessage("Obrigado por jogar! Até a próxima.");
-      break;
-
-    default:
-      displayMessage("Comando inválido.");
-      break;
+const countries = [
+  {
+    name: 'Vitória',
+    description: 'TERRAAAAA COM AGUA'
+  },
+  {
+    name: 'Vila Velha',
+    description: 'Terra do KN'
+  },
+  {
+    name: 'Cariacica',
+    description: 'Bosta seca'
+  },
+  {
+    name: 'Serra',
+    description: 'Assalto'
+  },
+  {
+    name: 'Viana',
+    description: 'Ta la né'
   }
-  
-  inputElement.value = "";
-}
-  
+];
 
-// Limpar o output ao recarregar a página
-window.addEventListener("load", () => {
-  outputElement.innerHTML = "";
+const travelButton = document.getElementById('travel-button');
+const hintButton = document.getElementById('hint-button');
+const resultElement = document.getElementById('result');
+const hintElement = document.getElementById('hint');
+
+let targetCountry; 
+
+function getRandomCountry() {
+  const randomIndex = Math.floor(Math.random() * countries.length);
+  return countries[randomIndex];
+}
+
+function generateHint(targetCountry) {
+  return `Pista: ${targetCountry.description}`;
+}
+
+hintButton.addEventListener('click', () => {
+  targetCountry = getRandomCountry();
+  const hint = generateHint(targetCountry);
+  hintElement.textContent = hint;
+  resultElement.textContent = ''; 
+});
+
+travelButton.addEventListener('click', () => {
+  const guessedCountry = prompt('Para onde você acha que o ladrão viajou?');
+  
+  if (guessedCountry === targetCountry.name) {
+    resultElement.textContent = 'Parabéns! Você pegou o ladrão!';
+  } else {
+    resultElement.textContent = `Oops! O ladrão escapou para ${targetCountry.name}. Tente novamente!`;
+  }
 });
